5b26aa702211ffe9478fb672afce1d3bacc104b5
durjyo8888 <kdurjyodhan141@gmail.com>
2023-09-08 18:48:49 +0530

    done

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -72,6 +73,7 @@ public class WhatsappController {
 
     @DeleteMapping("/remove-user")
     public int removeUser(User user) throws Exception{
+        //This is a bonus problem and does not contains any marks
         //A user belongs to exactly one group
         //If user is not found in any group, throw "User not found" exception
         //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
@@ -83,7 +85,7 @@ public class WhatsappController {
 
     @GetMapping("/find-messages")
     public String findMessage(Date start, Date end, int K) throws Exception{
-        // This is a bonus problem and does not contains any marks
+        //This is a bonus problem and does not contains any marks
         // Find the Kth latest message between start and end (excluding start and end)
         // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
 
